8980c8287785f78ffe33fbfa5dec769a2186cef6
Ahmed Ayad <aahmed@msimbo.com>
2021-02-23 17:52:34 -0500

    fix indent

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,9 +3,9 @@ import {Footer, MainNav} from "../components/ui";
 export const Home = () => (
     <>
         <MainNav/>
-        <div className="p-24">
-            <h1 className="text-4xl">Welcome to ASIC IP Finder</h1>
-        </div>
+            <div className="p-24">
+                <h1 className="text-4xl">Welcome to ASIC IP Finder</h1>
+            </div>
         <Footer/>
     </>
 )
